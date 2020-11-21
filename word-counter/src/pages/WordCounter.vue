ada7ccce01d40490c73c27cc786aff3771b09d6c
Vinicius Alves <vinicius.alves.contato@gmail.com>
2020-11-20 23:31:19 -0300

    Added a bit of styling

--- word-counter/src/pages/WordCounter.vue
+++ word-counter/src/pages/WordCounter.vue
@@ -1,15 +1,16 @@
 <template>
   <v-container fluid fill-height>
     <v-row align-center justify="center">
-      <v-card>
+      <v-card width="75%">
         <v-card-title class="justify-center text-h3 pa-10"> Word Counter </v-card-title>
-        <v-card-subtitle>
+        <v-card-subtitle class="text-center">
           Type a text in the form below to count how many words it contains!
         </v-card-subtitle>
         <v-card-text>
           <v-form>
             <v-row justify="center">
-              <v-textarea solo label="Type your text here..."> </v-textarea>
+              <v-textarea outlined no-resize label="Type your text here" class="pa-10">
+              </v-textarea>
             </v-row>
             <v-row justify="center">
               <v-btn class="justify-center">Count!</v-btn>
