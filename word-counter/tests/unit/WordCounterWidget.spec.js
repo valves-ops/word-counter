0d7297843eb29a75c5545723e16a13837d66aa74
Vinicius Alves <vinicius.alves.contato@gmail.com>
2020-11-21 15:01:35 -0300

    Added axios and new test

--- word-counter/tests/unit/WordCounterWidget.spec.js
+++ word-counter/tests/unit/WordCounterWidget.spec.js
@@ -1,6 +1,7 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import Vuetify from 'vuetify'
 import WordCounterWidget from '@/components/WordCounterWidget.vue'
+import axios from 'axios'
 // import vuetify from '../../src/plugins/vuetify'
 
 describe('WordCounterWidget.vue', () => {
@@ -22,5 +23,17 @@ describe('WordCounterWidget.vue', () => {
     textInput.trigger('input')
     wrapper.find('#count-button').trigger('click')
     expect(wrapper.vm.wordCount).toBe(3)
+  })
+
+  it('counts lorem ipsum', async () => {
+    let wordCount = 1 + Math.floor(99 * Math.random())
+    let response = await axios.get('http://www.randomtext.me/api/lorem/p-1/' + wordCount)
+    let text = response.data.text_out.replace('<p>', '').replace('</p>', '')
+
+    const textInput = wrapper.find('#text-input-field')
+    textInput.element.value = text
+    textInput.trigger('input')
+    wrapper.find('#count-button').trigger('click')
+    expect(wrapper.vm.wordCount).toBe(wordCount)
   })
 })
